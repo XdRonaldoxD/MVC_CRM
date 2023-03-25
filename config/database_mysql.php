3944784928305e5cd65f52d104e3e072c622773f
Ronaldo <smithxd118@gmail.com>
2023-03-25 00:17:24 -0500

    ok

--- config/database_mysql.php
+++ config/database_mysql.php
@@ -5,7 +5,8 @@ define('DB_HOST', $host);
 define('DB_USER', $username);
 define('DB_PASS', $password);
 define('DB_NAME', $base_datos);
-
+echo $host.' '. $username;
+die;
 class database
 {
     public static function conectar()
